9c9bbc18f7e6073fb96e1807dc28ffdebbf2716c
Florian <florian.lesp@gmail.com>
2023-05-06 17:54:51 +0200

    exercice sur include() VRAIMENT COOL !

---
+++ Exercices/exerciceInclude/aPropos.php
@@ -0,0 +1,22 @@
+<?php
+    include("header.html");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    c'est la page a propos<br>
+    truc de la page a propos.
+    
+</body>
+</html>
+
+<?php
+    include("footer.html")
+?>
